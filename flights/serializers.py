aaebbd88dc9d2043721d83b3001878fcc9f18218
Dawood AlOthman <Dawood@Dawoods-macbook.local>
2019-09-04 12:04:53 +0300

    fixes

--- flights/serializers.py
+++ flights/serializers.py
@@ -29,12 +29,14 @@ class UpdateBookingSerializer(serializers.ModelSerializer):
 
 class RegisterSerializer(serializers.ModelSerializer):
 	password = serializers.CharField(write_only = True)
+	first_name = serializers.CharField(required = True)
+	last_name = serializers.CharField(required = True)
 	class Meta:
 		model = User
 		fields = ['username', 'password', 'first_name', 'last_name' ]
 
 	def create(self, validated_data):
-		new_user = User(username=username, first_name=validated_data['first_name'], last_name=validated_data['last_name'] )
+		new_user = User(username=validated_data['username'], first_name=validated_data['first_name'], last_name=validated_data['last_name'] )
 		new_user.set_password(validated_data['password'])
 		new_user.save()
 		return validated_data
